6776c468c6131790d7d18eaed559bb74ef4c717c
Duckida <61168649+duckida@users.noreply.github.com>
2020-07-31 14:47:40 -0700

    Update main.py

--- Mica/main.py
+++ Mica/main.py
@@ -16,7 +16,6 @@ class Mica(object):
  
       #  Called when we connect to a mica.
 
-
         print("Connected to a Mica named %s." % (mica.name))
         Thread(target=self.micaThread, args=(mica,)).start()
 
@@ -35,17 +34,21 @@ class Mica(object):
                 strCmd = str(cmd)
 
                 if strCmd == "Forward":
-                    mica.cmds.body.do_forward(30, 10)
+                    mica.cmds.body.do_forward(60, 10)
                     
                 elif strCmd == "Back":
-                    mica.cmds.body.do_forward(-30, 10)
+                    mica.cmds.body.do_forward(-60, 10)
                     
                 elif strCmd == "Right":
                     mica.cmds.body.do_turn(-90, 200)
                     
                 elif strCmd == "Left":
                     mica.cmds.body.do_turn(90, 200)
-        elif strMode == "Free Time":
+                elif strCmd == "Exit":
+                    break
+        elif strMode == "Free Time": 
+
+          print("%s is so happy that he gets free time!" % mica.name)
           while True:
             #The variable used for AI
             
@@ -54,7 +57,6 @@ class Mica(object):
             ftm2 = random.randint(0, 20)
             ftm3 = random.randint(0, 20)
             ftm4 = random.randint(0, 20)
-            print("%s is so happy that he gets free time!" % mica.name)
 
             if ftm == 1:
                 if ftm1 > 19:
@@ -64,7 +66,10 @@ class Mica(object):
             elif ftm == 2:
                 print("%s is exploring his surroundings" % mica.name)
                 mica.cmds.body.do_forward(30, 10)
-                mica.cmds.body.do_turn(90, 200)
+                if ftm2 == 10:
+                    mica.cmds.body.do_turn(90, 200)
+                else:
+                    mica.cmds.body.do_turn(-90, 200)
         
             elif ftm == 3:
                 print("%s is dizzy" % mica.name)
@@ -72,7 +77,73 @@ class Mica(object):
                 
             elif ftm == 4:
                 print("%s wants to play with you" % mica.name)
-                                
+
+            elif ftm == 5:
+              if ftm2 <= 10:
+                print("%s wants to play Control Mode" % mica.name)
+                rawDTP = raw_input("Do you want to play Control Mode with %s? " % mica.name)
+                dtp = str(rawDTP)
+                if dtp == "Yes":
+                    print("Opening control mode..")
+                    while True:
+                        cmd = raw_input("%s ready! >" % mica.name)
+                        strCmd = str(cmd)
+
+                        if strCmd == "Forward":
+                        mica.cmds.body.do_forward(60, 10)
+                    
+                        elif strCmd == "Back":
+                            mica.cmds.body.do_forward(-60, 10)
+                    
+                        elif strCmd == "Right":
+                            mica.cmds.body.do_turn(-90, 200)
+                    
+                        elif strCmd == "Left":
+                            mica.cmds.body.do_turn(90, 200)
+                        elif strCmd == "Exit":
+                            break
+            elif ftm == 6:
+                print("%s is playing Disco!" % mica.name)
+                lgt = random.randint(0, 1)
+                lgt2 = random.randint(0, 1)
+                lgt1 = random.randint(0, 1)
+                mica.cmds.RGB.stage_all(lgt2, lgt, lgt1)
+                time.sleep(0.5)
+                lgt = random.randint(0, 1)
+                lgt2 = random.randint(0, 1)
+                lgt1 = random.randint(0, 1)
+                mica.cmds.RGB.stage_all(lgt2, lgt, lgt1)
+                time.sleep(0.5)
+                lgt = random.randint(0, 1)
+                lgt2 = random.randint(0, 1)
+                lgt1 = random.randint(0, 1)
+                mica.cmds.RGB.stage_all(lgt2, lgt, lgt1)
+                time.sleep(0.5)
+                lgt = random.randint(0, 1)
+                lgt2 = random.randint(0, 1)
+                lgt1 = random.randint(0, 1)
+                mica.cmds.RGB.stage_all(lgt2, lgt, lgt1)
+                time.sleep(0.5)
+                lgt = random.randint(0, 1)
+                lgt2 = random.randint(0, 1)
+                lgt1 = random.randint(0, 1)
+                mica.cmds.RGB.stage_all(lgt2, lgt, lgt1)
+                time.sleep(0.5)
+            elif ftm == 7:
+                print("%s is hungry" % mica.name)
+                rawFood = raw_input("Would you like to feed %s?" % mica.name)
+                foodD = str(rawFood)
+                if foodD == "Yes":
+                    foodList = ["Nuts and Bolts", "Cereal", "Mango", "Pasta", "Ice Cream", "Popsicle", "Honeydew", "Almonds"]
+                    ciList = random.randint(1,8)
+                    print("Feeding Mica %s" % foodList[ciList])
+                    print(".")
+                    print("..")
+            
+                
+                
+                
+                  
                 
            
            
